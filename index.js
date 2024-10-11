dd902daa35deb6b02e58df37e47846213cfccb99
Bruno-devf <fonsecabruno6062@gmailcom>
2024-10-10 21:43:54 -0300

    upd

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require("express")
-const server = express()
-const usuario = require("./scr/teste.json")
-const eventos = require("./scr/teste1.json")
+const express = require("express");
+const server = express();
+const usuario = require("./scr/teste.json");
+const evento = require("./scr/teste1.json");
 
 server.get("/usuario", (req, res) =>{
     return res.json({usuario})
-})
-server.get("/evento", (req, res) =>{
-    return res.json({eventos}) 
-})
+});
+server.get("/eventos", (req, res) =>{
+    return res.json({evento}) 
+});
 
+server.get("/teste/:email/:senha", (req, res) =>{
+    return res.send(req.params) 
+});
 
 server.listen(6969, ()=>{
     console.log("servidor on")
-})
+});
